65ff5e79016e718e50d0a45f9ed31dc6f44464b5
Gregory Sheygam <sheygam@gmail.com>
2021-03-11 11:15:28 +0200

    added components to app-component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import LoginPage from './components/LoginPage';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="App">
-     
+     <TodoList/>
+     <LoginPage/>
     </div>
   );
 }
